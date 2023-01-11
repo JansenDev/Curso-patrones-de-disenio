7ee23ddedb589f7e7f730995eb2091d5ffe54b14
JHONATAN JANSSEN SEGURA GALINDO <jsegura-e@ripley.com.pe>
2023-01-10 20:09:42 -0500

    added: reforzando patron decorador

--- decorador/decorador.ts
+++ decorador/decorador.ts
@@ -1,95 +1,44 @@
-/**
- * The base Component interface defines operations that can be altered by
- * decorators.
- */
 interface Component {
-    operation(): string;
+    operation(): string
 }
 
-/**
- * Concrete Components provide default implementations of the operations. There
- * might be several variations of these classes.
- */
-class ConcreteComponent implements Component {
-    public operation(): string {
-        return 'ConcreteComponent';
+class DefaultComponent implements Component {
+    operation(): string {
+        return `Default component`
     }
-}
 
-/**
- * The base Decorator class follows the same interface as the other components.
- * The primary purpose of this class is to define the wrapping interface for all
- * concrete decorators. The default implementation of the wrapping code might
- * include a field for storing a wrapped component and the means to initialize
- * it.
- */
+}
 class Decorator implements Component {
-    protected component: Component;
-
-    constructor(component: Component) {
-        this.component = component;
-    }
-
-    /**
-     * The Decorator delegates all work to the wrapped component.
-     */
-    public operation(): string {
-        return this.component.operation();
+    constructor(private component: Component) { }
+    operation(): string {
+        return this.component.operation()
     }
 }
 
-/**
- * Concrete Decorators call the wrapped object and alter its result in some way.
- */
 class ConcreteDecoratorA extends Decorator {
-    /**
-     * Decorators may call parent implementation of the operation, instead of
-     * calling the wrapped object directly. This approach simplifies extension
-     * of decorator classes.
-     */
-    public operation(): string {
-        return `ConcreteDecoratorA(${super.operation()})`;
+    operation(): string {
+        return `ConcreteComponentA(${super.operation()})`
     }
 }
 
-/**
- * Decorators can execute their behavior either before or after the call to a
- * wrapped object.
- */
 class ConcreteDecoratorB extends Decorator {
-    public operation(): string {
-        return `ConcreteDecoratorB(${super.operation()})`;
+
+    operation(): string {
+        return `ConcreteComponentB(${super.operation()})`
     }
+
 }
 
-/**
- * The client code works with all objects using the Component interface. This
- * way it can stay independent of the concrete classes of components it works
- * with.
- */
-function clientCode(component: Component) {
-    // ...
+const comp = new DefaultComponent()
 
-    console.log(`RESULT: ${component.operation()}`);
+console.log("[Result]");
+console.log(comp.operation());
 
-    // ...
-}
+const dec1 = new ConcreteDecoratorA(comp)
+console.log("[Result]");
+console.log(dec1.operation());
 
-/**
- * This way the client code can support both simple components...
- */
-const simple = new ConcreteComponent();
-console.log('Client: I\'ve got a simple component:');
-clientCode(simple);
-console.log('');
+const dec2 = new ConcreteDecoratorB(dec1)
+console.log("[Result]");
+console.log(dec2.operation());
 
-/**
- * ...as well as decorated ones.
- *
- * Note how decorators can wrap not only simple components but the other
- * decorators as well.
- */
-const decorator1 = new ConcreteDecoratorA(simple);
-const decorator2 = new ConcreteDecoratorB(decorator1);
-console.log('Client: Now I\'ve got a decorated component:');
-clientCode(decorator2);
