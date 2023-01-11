bd7031d01643cc52ed67f0271cb972407f54183c
JHONATAN JANSSEN SEGURA GALINDO <jsegura-e@ripley.com.pe>
2023-01-10 20:42:49 -0500

    added: patron decorador simulando envio de correos

--- decorador/decorador.ts
+++ decorador/decorador.ts
@@ -1,44 +1,61 @@
 interface Component {
-    operation(): string
+    send(email: IEmail): IEmail
 }
 
-class DefaultComponent implements Component {
-    operation(): string {
-        return `Default component`
+interface IEmail {
+    name: string
+    email: string
+    subject: string
+}
+
+class SendGmailMessage implements Component {
+    send(email: IEmail): IEmail {
+        console.log(`Send Gmail message to :${email.email}`);
+        return email
     }
 
 }
 class Decorator implements Component {
     constructor(private component: Component) { }
-    operation(): string {
-        return this.component.operation()
+    send(email: IEmail): IEmail {
+        return this.component.send(email)
     }
 }
 
-class ConcreteDecoratorA extends Decorator {
-    operation(): string {
-        return `ConcreteComponentA(${super.operation()})`
+class SendFacebookMessageDecorator extends Decorator {
+    send(email: IEmail): IEmail {
+        super.send(email)
+        console.log(`Send facebook message to: ${email.email}`);
+        return email
     }
 }
 
-class ConcreteDecoratorB extends Decorator {
+class SendSlackMessageDecorator extends Decorator {
 
-    operation(): string {
-        return `ConcreteComponentB(${super.operation()})`
+    send(email: IEmail): IEmail {
+        super.send(email)
+        console.log(`Send slack message to: ${email.email}`);
+        return email
     }
 
 }
 
-const comp = new DefaultComponent()
+
+const email: IEmail = {
+    email: "seguragjj25@gmail.com",
+    name: "jhonatan",
+    subject: "Greeting"
+}
+const emailMessage = new SendGmailMessage()
 
 console.log("[Result]");
-console.log(comp.operation());
+emailMessage.send(email)
 
-const dec1 = new ConcreteDecoratorA(comp)
+const facebookMessage = new SendFacebookMessageDecorator(emailMessage)
 console.log("[Result]");
-console.log(dec1.operation());
+facebookMessage.send(email)
 
-const dec2 = new ConcreteDecoratorB(dec1)
+const slackMessage = new SendSlackMessageDecorator(facebookMessage)
 console.log("[Result]");
-console.log(dec2.operation());
+slackMessage.send(email)
 
